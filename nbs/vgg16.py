02d82c1b55352b04a642810d33d61e3e0dd2935c
Ubuntu <ubuntu@ip-10-0-0-12.us-west-2.compute.internal>
2017-08-02 21:55:07 +0000

    compcars notebook updates

--- nbs/vgg16.py
+++ nbs/vgg16.py
@@ -209,8 +209,9 @@ class Vgg16():
             Fits the model on data yielded batch-by-batch by a Python generator.
             See Keras documentation: https://keras.io/models/model/
         """
-        self.model.fit_generator(batches, samples_per_epoch=batches.nb_sample, nb_epoch=nb_epoch,
+        history = self.model.fit_generator(batches, samples_per_epoch=batches.nb_sample, nb_epoch=nb_epoch,
                 validation_data=val_batches, nb_val_samples=val_batches.nb_sample)
+        return history
 
 
     def test(self, path, batch_size=8):
