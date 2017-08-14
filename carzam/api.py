15e1f1800abb065e636f09b3c5abe959a38a1482
Gohar Irfan Chaudhry <goharirfan94@gmail.com>
2017-08-14 21:57:11 +0000

    api endpoint working for hardcoded input image

--- carzam/api.py
+++ carzam/api.py
@@ -1,7 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
 import single_prediction as pred
-
-batches, model_vgg19, model_inception_v1 = get_loaded_models_and_batches(img_rows, img_cols, channel, batch_size, data_path, model_path, imagenet_model_path)
 
 img_rows = 224
 img_cols = 224
@@ -11,14 +9,18 @@ data_path = "../data/compcars/data/image/"
 model_path = "../models/"
 imagenet_model_path = "../imagenet_models/"
 
+batches, model_vgg19, model_inception_v1 = pred.get_loaded_models_and_batches(img_rows, img_cols, channel, batch_size, data_path, model_path, imagenet_model_path)
+
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    img = get_image(img_rows, img_cols, "mini.png")
-    classes, classes_top_5 = perform_combined_prediction(model_vgg19, model_inception_v1, batches, img)
+    img = pred.get_image(img_rows, img_cols, "mini.png")
+    classes, classes_top_5 = pred.perform_combined_prediction(model_vgg19, model_inception_v1, batches, img)
     print classes
     print classes_top_5
+    response = jsonify(classes=classes, classes_top_5=classes_top_5)
+    return response
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8888)
